8e577861aa3691dc0d53b4cdcb24822dfd71796f
ni_n_ja <rampage4extrm@gmail.com>
2016-05-18 19:05:57 +0900

    add UI

--- public/js/web.js
+++ public/js/web.js
@@ -1,45 +1,42 @@
 $(document).ready(function() {
-    $(".moji").hover(function() {
-        $("#" + $(this).attr("id") + "svg").stop().velocity({
-            "fill-opacity": 1,
-            "stroke-opacity": 0
-        }, 100);
-        $("#" + $(this).attr("id") + "svg2").stop().velocity({
-            "fill-opacity": 0,
-            "stroke-opacity": 1
-        }, 100);
-    }, function() {
-        $("#" + $(this).attr("id") + "svg").stop().velocity({
-            "fill-opacity": 0,
-            "stroke-opacity": 1
-        }, 100);
-        $("#" + $(this).attr("id") + "svg2").stop().velocity({
-            "fill-opacity": 1,
-            "stroke-opacity": 0
-        }, 100);
-    });
+    var wW = $(window).width();
+    var wH = $(window).height();
+    $('#canvas').attr("width", wW);
+    $('#canvas').attr("height", wH);
+    var init = {
+        oX: $('.circle').offset().left,
+        oY: $('.circle').offset().top,
+        x: $('.circle').width(),
+        y: $('.circle').height(),
+        r: $('.circle').width() / 2,
+        roundX: $('.round').offset().left + $('.round').width() / 2,
+        roundY: $('.round').offset().top + $('.round').height() / 2
+    };
+    var canvas = document.getElementById("canvas");
+    console.log(JSON.stringify(init), wW, wH);
+    $(document).mousemove(function(e) {
+        var context = canvas.getContext('2d');
+        context.clearRect(0, 0, wW, wH);
+        context.strokeStyle = 'rgb(255,255,255)'; //枠線の色は青
+        context.beginPath();
+        context.moveTo(init.roundX, init.roundY);
+        var x = e.clientX;
+        var y = e.clientY;
 
-    $("html").bind("click", function(e) {
-        if ($("#srkn").offset().left <= -90 && $("#onigiri").offset().left <= -90) {
-            var rand = Math.floor(Math.random() * 100);
-            if (rand < 80) {
-                $("#srkn").css("top", (e.clientY - 40) + 'px');
-                $("#srkn").velocity({
-                    "translateX": $(window).width() + 90,
-                    "easing": "easeOutQuad"
-                }, 800).velocity({
-                    "translateX": 0
-                }, 0);
-            } else {
-                $("#onigiri").css("top", (e.clientY - 40) + 'px');
-                $("#onigiri").velocity({
-                    "translateX": $(window).width() + 90,
-                    "easing": "easeOutQuad"
-                }, 1200).velocity({
-                    "translateX": 0
-                }, 0);
-            }
+        var distance = Math.sqrt((x - init.roundX) * (x - init.roundX) + (y - init.roundY) * (y - init.roundY));
+        if (distance > 50) {
+            distance = 50;
         }
-
+        var rad = Math.atan2(x - init.roundX, y - init.roundY);
+        var fixedX = distance * Math.sin(rad);
+        var fixedY = distance * Math.cos(rad);
+        //context.lineTo(x, y);
+        context.lineTo(init.roundX + fixedX, init.roundY + fixedY);
+        context.closePath();
+        context.stroke();
+        $('.circle').offset({
+            left: (fixedX + init.oX),
+            top: (fixedY + init.oY)
+        });
     });
 });
